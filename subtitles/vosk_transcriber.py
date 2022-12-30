83106b02e96bee340119f1bb46ae2a61b4d05e24
Matthias Reumann <matthias.reumann@tum.de>
2022-12-30 06:41:28 +0100

    Remove mutex in VoskTranscriber
    Thanks @nshmyrev

--- subtitles/vosk_transcriber.py
+++ subtitles/vosk_transcriber.py
@@ -1,11 +1,7 @@
 import subprocess
-
 import vosk
 from vosk import Model, KaldiRecognizer, SetLogLevel
 from transcriber import Transcriber
-from threading import Lock
-
-mutex_vosk = Lock()
 
 
 class VoskTranscriptionError(Exception):
@@ -23,9 +19,7 @@ class VoskTranscriber(Transcriber):
                 Visit https://alphacephei.com/vosk/models for a list of available models.
         """
         super().__init__()
-        self.__recognizers = {
-            model['lang']: new_recognizer(model['path'])
-            for model in models}
+        self.__models = {model['lang']: model['path'] for model in models}
 
     def generate(self, source: str, language: str) -> (str, str):
         with subprocess.Popen(['ffmpeg',
@@ -35,12 +29,9 @@ class VoskTranscriber(Transcriber):
                                '-ac', '1',
                                '-f', 'wav', "-"],
                               stdout=subprocess.PIPE).stdout as stream:
-            if language in self.__recognizers:
-                mutex_vosk.acquire()
-                try:
-                    return self.__recognizers[language].SrtResult(stream), language
-                finally:
-                    mutex_vosk.release()
+            if language in self.__models:
+                recognizer = new_recognizer(self.__models[language])
+                return recognizer.SrtResult(stream), language
 
             else:
                 raise VoskTranscriptionError(f'Unsupported language: {language}')
